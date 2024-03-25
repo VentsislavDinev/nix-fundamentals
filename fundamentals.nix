e301cf876f70dfb80595adf8c3bf6cb2eb241bb4
Hristo Krasenov <hristo.krasenov@metacraft-labs.com>
2024-03-25 18:04:48 +0200

    Simple Update

--- fundamentals.nix
+++ fundamentals.nix
@@ -1,32 +1,60 @@
 # fundamentals.nix
-
 let
-GitRepo = { url, auth, path }:
-Commit = { hash, message, author, timestamp, files }:
-Timestamp = 1;
-Hash = abcdef123456:
-UserInfo = { name: john, email: "john@john.com" }:
-Tag = { name: john, commit: Commit, tagger?: UserInfo, date?: Timestamp}:
-MermaidDiagram = 
-extractAttributes = builtins.fetchGit {
-  url = "git@github.com:VentsislavDinev/nix-fundamentals.git";
-  ref = "master";
-  rev = "adab8b916a45068c044658c4158d81878f9ed1c3";
-};
-in 
-extractAttributes
+  GitRepo = {
+    url,
+    auth,
+    path,
+  }: {inherit url auth path;};
+  Commit = {
+    hash,
+    message,
+    author,
+    timestamp,
+    files,
+  }: {inherit hash message author timestamp files;};
+  Timestamp = 1;
+  Hash = "abcdef123456";
+  UserInfo = {
+    name ? "john",
+    email ? "john@john.com",
+  }: {inherit name email;};
+  Tag = {
+    name ? UserInfo.name,
+    commit ? Commit,
+    tagger ? UserInfo,
+    date ? Timestamp,
+  }: {inherit name commit tagger date;};
+  MermaidDiagram = "some_string";
+  # extractAttributes = builtins.fetchGit {
+  #   url = "";
+  #   ref = "";
+  #   rev = "";
+  # };
+in {
+  inherit
+    # extractAttributes
+    GitRepo
+    Commit
+    Timestamp
+    Hash
+    UserInfo
+    Tag
+    MermaidDiagram
+    ;
+}
 
+#GitRepo functions
 
+#* getGitHubRepo :: { orgName: string, repoName: string } -> GitRepo 
+#* getBranches :: (repo: GitRepo) → [ string ] 
+#* getCommits :: (repo: GitRepo) → (branchName: string) → [ Commit ] 
+#* getTags :: (repo: GitRepo) → [ Tag ] 
+#* generateMermaidVisualization :: GitRepo → MermaidDiagram 
 
-  ## Code block from a previous task for reference
-  ## nimbleLock = builtins.fromJSON (builtins.readFile ./nimble.lock); #Read the contents of the nimble.lock file parse it as JSON
-  ## getPackages = builtins.attrValues nimbleLock.packages; #Extract the values from the 'packages' attribute of the parsed nimbleLock
-  ## builtins 
-  ## extractAttributes = builtins.attrValues (builtins.mapAttrs (name: value: #Define a function to exctract attributes from each package
-  ##    {
-  ##    name = name; #Assign the 'name' attribute from the package to the same attribute in the result name = name;
-  ##    hash = value.vcsRevision; #Assign the 'vcsRevision' attribute from the package as 'hash' in the resulthash = value.vcsRevision;
-  ##    url = value.url; #Assign the 'url' attribute from the package to the same attribute in the result url = value.url;
-  ##    }
-  ## ) nimbleLock.packages
-  ## ); in extractAttributes
+#Commit functions
+
+#* getCommitHash :: Commit → Hash 
+#* getParentCommits :: Commit → [ Commit ] 
+#* getAuthor :: Commit → UserInfo 
+#* getCommitter :: Commit → UserInfo 
+#* getMessage :: Commit → { title: string, body: string } 
---
+++ gitrepo.nix
@@ -0,0 +1,55 @@
+# The functions below are GitRepo functions
+
+# In this case the value of the geGitHubRepo is an object containing two fields
+  getGitHubRepo = { 
+    orgName = "metacraft-labs";
+    repoName = "$(basename $(git rev-parse --show-toplevel))";
+  };
+
+  getBranches = { 
+    repoName = "$(git rev-parse --show-toplevel)";
+    branchName = "$(git branch | head -n 1)";
+  };
+
+  getCommits = { 
+    repoName = "$(git rev-parse --show-toplevel)";
+    commit_one = "$(git lg -n 1 --skip=3 --date-order)"; #Shows 231ab18 (Oldest Commit)
+    commit_two = "$(git lg -n 1 --skip=2 --date-order)"; #Shows 73616ab 
+    commit_three = "$(git lg -n 1 --skip=1 --date-order)"; #Shows 897ad29
+    commit_four = "$(git lg -n 1 --date-order)"; #Shows 9db6651 (Newest Commit)
+  };
+
+  getTags = { 
+    repoName = "$(git rev-parse --show-toplevel)";
+    tagName_one = "$(git tag --list | sed -n 1p)"; #v1.0
+    tagName_two = "$(git tag --list | sed -n 2p)"; #v1.1
+    tagName_three = "$(git tag --list | sed -n 3p)"; #v1.3
+  };
+
+  generateMermaidVisualization = { 
+    orgName = "metacraft-labs";
+    repoName = "$(git rev-parse --show-toplevel)";
+  };
+
+# The functions below are Commit functions
+
+# The hashes below are gotten by the git rev-parse {The_Name_Of_The_Commit}
+# This command is used to convert a Git object name (like 231ab18) into a form that is readable and usable by other Git commands.
+  getCommitHash = { 
+    commit_one_hash = "$(git rev-parse $(commit_one))"; #231ab1829e9ef2433629e2b7c565370f63b4401b
+    commit_two_hash = "$(git rev-parse $(commit_two))"; #73616ab9dfb1773d816ee53004f56795f87728e3
+    commit_three_hash = "$(git rev-parse $(commit_three))"; #897ad292078780ef406a161d7925d0b0b9da126a
+    commit_four_hash = "$(git rev-parse $(commit_four))"; #9db665144c2aa5313bd3f31e7596c77011645881
+  };
+
+  getParentCommits = {
+    grandFather = "$(git log --pretty=format:%P -n 1 $(commit_one)^@) && echo -n 'This commit has no parent!'";
+    normalFather = "$(git log --pretty=format:%P -n 1 $(commit_two)^@)"; #Shows 231ab1829e9ef2433629e2b7c565370f63b4401b
+    normalSon = "$(git log --pretty=format:%P -n 1 $(commit_three)^@)"; #Shows 73616ab9dfb1773d816ee53004f56795f87728e3
+    grandSon = "$(git log --pretty=format:%P -n 1 $(commit_four)^@)"; #Shows 897ad292078780ef406a161d7925d0b0b9da126a
+  };
+
+  getAuthor = { 
+    orgName = "metacraft-labs";
+    repoName = "$(git rev-parse --show-toplevel)";
+  };
