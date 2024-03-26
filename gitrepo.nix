0c84b0566f4e40a0f9fdc079e92e890e6120e4b1
Hristo Krasenov <hristo.krasenov@metacraft-labs.com>
2024-03-26 12:09:47 +0200

    More Update

--- gitrepo.nix
+++ gitrepo.nix
@@ -36,10 +36,10 @@
 # The hashes below are gotten by the git rev-parse {The_Name_Of_The_Commit}
 # This command is used to convert a Git object name (like 231ab18) into a form that is readable and usable by other Git commands.
   getCommitHash = { 
-    commit_one_hash = "$(git rev-parse $(commit_one))"; #231ab1829e9ef2433629e2b7c565370f63b4401b
-    commit_two_hash = "$(git rev-parse $(commit_two))"; #73616ab9dfb1773d816ee53004f56795f87728e3
-    commit_three_hash = "$(git rev-parse $(commit_three))"; #897ad292078780ef406a161d7925d0b0b9da126a
-    commit_four_hash = "$(git rev-parse $(commit_four))"; #9db665144c2aa5313bd3f31e7596c77011645881
+    commit_one_hash = "$(git rev-parse $(commit_one))"; #Shows 231ab1829e9ef2433629e2b7c565370f63b4401b
+    commit_two_hash = "$(git rev-parse $(commit_two))"; #Shows 73616ab9dfb1773d816ee53004f56795f87728e3
+    commit_three_hash = "$(git rev-parse $(commit_three))"; #Shows 897ad292078780ef406a161d7925d0b0b9da126a
+    commit_four_hash = "$(git rev-parse $(commit_four))"; #Shows 9db665144c2aa5313bd3f31e7596c77011645881
   };
 
   getParentCommits = {
@@ -50,6 +50,23 @@
   };
 
   getAuthor = { 
-    orgName = "metacraft-labs";
-    repoName = "$(git rev-parse --show-toplevel)";
+    author_of_first_commit = "$(git log -1 --format='%an' $(commit_one_hash))";
+    author_of_second_commit = "$(git log -1 --format='%an' $(commit_two_hash))";
+    author_of_third_commit = "$(git log -1 --format='%an' $(commit_three_hash))"
+    author_of_fourth_commit = "$(git log -1 --format='%an' $(commit_four_hash))"
   };
+
+  getCommiter = { 
+    commiter_of_the_first_commit = "$(git log -1 --format='%cn' $(commit_one_hash))";
+    commiter_of_the_second_commit = "$(git log -1 --format='%cn' $(commit_two_hash))";
+    commiter_of_the_third_commit = "$(git log -1 --format='%cn' $(commit_three_hash))";
+    commiter_of_the_fourth_commit = "$(git log -1 --format='%cn' $(commit_four_hash))";
+  };
+
+  getMessage = { 
+    message_of_first_commit = "$(git log -1 --format='%s' $(commit_one_hash))";
+    message_of_second_commit = "$(git log -1 --format='%s' $(commit_two_hash))";
+    message_of_third_commit = "$(git log -1 --format='%s' $(commit_three_hash))";
+    message_of_fourth_commit = "$(git log -1 --format='%s' $(commit_four_hash))";
+  };
+
