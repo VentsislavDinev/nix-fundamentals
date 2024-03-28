e56d7fb696e4b0f312242a3e8a57ee918af3fe2e
Hristo Krasenov <hristo.krasenov@metacraft-labs.com>
2024-03-28 14:44:59 +0200

    Add more Nix Values

--- nix-values/default.nix
+++ nix-values/default.nix
@@ -2,7 +2,7 @@ rec {
   path = ./string.nix;
   value = import string.nix;
   type = "string";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type";
   isCorrect = checkFunction value;
 }
 
@@ -34,6 +34,38 @@ rec {
   path = ./null.nix;
   value = import ./null.nix;
   type = "null";
+  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  isCorrect = checkFunction value;
+}
+
+rec {
+  path = ./list.nix;
+  value = import ./list.nix;
+  type = "list";
+  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  isCorrect = checkFunction value;
+}
+
+rec {
+  path = ./attribute_set.nix;
+  value = import ./attribute_set.nix;
+  type = "set";
+  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  isCorrect = checkFunction value;
+}
+
+rec {
+  path = ./function.nix;
+  value = import ./function.nix;
+  type = "function";
+  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  isCorrect = checkFunction value;
+}
+
+rec {
+  path = ./derivation.nix;
+  value = import ./derivation.nix;
+  type = "derivation";
   checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
   isCorrect = checkFunction value;
 }
