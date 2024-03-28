ac1fe505909e4382d9c55f187eb7a28c22169369
Hristo Krasenov <hristo.krasenov@metacraft-labs.com>
2024-03-28 14:50:10 +0200

    Add values to checkFunction

--- nix-values/default.nix
+++ nix-values/default.nix
@@ -2,7 +2,7 @@ rec {
   path = ./string.nix;
   value = import string.nix;
   type = "string";
-  checkFunction = value: value ? "type";
+  checkFunction = value: value ? "type" && value.type == "string";
   isCorrect = checkFunction value;
 }
 
@@ -10,7 +10,7 @@ rec {
   path = ./integer.nix;
   value = import integer.nix;
   type = "integer";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "integer";
   isCorrect = checkFunction value;
 }
 
@@ -18,7 +18,7 @@ rec {
   path = ./boolean.nix;
   value = import ./boolean.nix;
   type = "boolean";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "boolean";
   isCorrect = checkFunction value;
 }
 
@@ -26,7 +26,7 @@ rec {
   path = ./path.nix;
   value = import path.nix;
   type = "path";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "path";
   isCorrect = checkFunction value;
 }
 
@@ -34,7 +34,7 @@ rec {
   path = ./null.nix;
   value = import ./null.nix;
   type = "null";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "null";
   isCorrect = checkFunction value;
 }
 
@@ -42,7 +42,7 @@ rec {
   path = ./list.nix;
   value = import ./list.nix;
   type = "list";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "list";
   isCorrect = checkFunction value;
 }
 
@@ -50,7 +50,7 @@ rec {
   path = ./attribute_set.nix;
   value = import ./attribute_set.nix;
   type = "set";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "set";
   isCorrect = checkFunction value;
 }
 
@@ -58,7 +58,7 @@ rec {
   path = ./function.nix;
   value = import ./function.nix;
   type = "function";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "function";
   isCorrect = checkFunction value;
 }
 
@@ -66,6 +66,6 @@ rec {
   path = ./derivation.nix;
   value = import ./derivation.nix;
   type = "derivation";
-  checkFunction = value: /* a function that checks whether the given `value` is of the correct type */;
+  checkFunction = value: value ? "type" && value.type == "derivation";
   isCorrect = checkFunction value;
 }
